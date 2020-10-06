c0fe60769a7b4e3d13c68dd4f8b873b695e81c4c
Olzindel <timo.thibault@gmail.com>
2020-10-07 01:23:37 +0200

    create education component

---
+++ src/components/Education/Education.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Education extends Component {
+    render() {
+        return (
+            <span>Hello, I'm the Education tab</span>
+        );
+    }
+}
